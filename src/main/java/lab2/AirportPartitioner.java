1adae6342c81972dd889dcda4c330b80259d4ebe
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-10-21 14:12:04 +0300

    Scripted auto-commit on change (2020-10-21 14:12:04) by gitwatch.sh

--- src/main/java/lab2/AirportPartitioner.java
+++ src/main/java/lab2/AirportPartitioner.java
@@ -5,5 +5,7 @@ import org.apache.hadoop.mapreduce.Partitioner;
 
 public class AirportPartitioner extends Partitioner<AirportID, Text> {
     @Override
-    public int getPartition(AirportID id, Text text, )
+    public int getPartition(AirportID obj, Text text, int number) {
+        return ((Integer.hashCode(obj.airportID) & Integer.MAX_VALUE) % number);
+    }
 }
