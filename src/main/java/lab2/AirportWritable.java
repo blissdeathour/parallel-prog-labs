7401b70d72b5050458b0dd5c84df4f8ec2b45151
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-10-21 10:42:22 +0300

    Scripted auto-commit on change (2020-10-21 10:42:22) by gitwatch.sh

--- src/main/java/lab2/AirportWritable.java
+++ src/main/java/lab2/AirportWritable.java
@@ -2,18 +2,22 @@ package lab2;
 
 import org.apache.hadoop.io.Writable;
 
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class AirportWritable implements Writable {
-    int     code;
-    String  description;
+    int code;
+    String description;
 
-    public AirportWritable() {};
+    public AirportWritable() {}
 
-    public AirportWritable(int code, String description)
-    {
+    public AirportWritable(int code, String description) {
         this.code = code;
         this.description = description;
     }
 
     @Override
-    public void write()
+    public void write(DataOutput out) throws IOException {
+        
+    }
 }
