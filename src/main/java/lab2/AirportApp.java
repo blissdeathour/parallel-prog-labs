05f76e265b4c85e800198a709a3902dd1711f550
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-10-21 15:07:07 +0300

    Scripted auto-commit on change (2020-10-21 15:07:07) by gitwatch.sh

--- src/main/java/lab2/AirportApp.java
+++ src/main/java/lab2/AirportApp.java
@@ -1,6 +1,7 @@
 package lab2;
 
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.TextInputFormat;
 import org.apache.hadoop.mapred.lib.MultipleInputs;
 import org.apache.hadoop.mapreduce.Job;
 
@@ -13,6 +14,6 @@ public class AirportApp {
         Job job = Job.getInstance();
         job.setJarByClass(AirportApp.class);
         job.setJobName("Flights Joiner");
-        MultipleInputs.addInputPath(job, new Path(args[0]), );
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
     }
 }
