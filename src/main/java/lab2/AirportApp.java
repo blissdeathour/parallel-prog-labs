33d9d963475977a4f989de97c18886385c01a62e
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-10-21 15:04:18 +0300

    Scripted auto-commit on change (2020-10-21 15:04:18) by gitwatch.sh

--- src/main/java/lab2/AirportApp.java
+++ src/main/java/lab2/AirportApp.java
@@ -1,11 +1,14 @@
 package lab2;
 
+import org.apache.hadoop.mapreduce.Job;
+
 public class AirportApp {
     public static void main(String[] args) throws Exception {
         if (args.length != 3)
         {
             System.exit(-1);
         }
+        Job job = Job.getInstance();
         
     }
 }
