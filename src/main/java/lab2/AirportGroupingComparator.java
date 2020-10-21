e51f49db9aa5e860ed049ff5750b1a0373b28415
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-10-21 14:27:59 +0300

    Scripted auto-commit on change (2020-10-21 14:27:59) by gitwatch.sh

--- src/main/java/lab2/AirportGroupingComparator.java
+++ src/main/java/lab2/AirportGroupingComparator.java
@@ -6,5 +6,7 @@ public class AirportGroupingComparator extends WritableComparator {
     AirportGroupingComparator() {
         super(AirportID.class, true);
     }
-    
+
+    @Override
+    public int compare(WritableComparator )
 }
