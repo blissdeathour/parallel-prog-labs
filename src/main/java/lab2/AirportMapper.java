df55c2fcd39a782e386750ca7d7f69c0b08fa0a5
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-10-21 11:58:37 +0300

    Scripted auto-commit on change (2020-10-21 11:58:37) by gitwatch.sh

--- src/main/java/lab2/AirportMapper.java
+++ src/main/java/lab2/AirportMapper.java
@@ -11,6 +11,6 @@ public class AirportMapper extends Mapper<LongWritable, Text, AirportID, IntWrit
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
-        
+        if (key.get() !=)
     }
 }
