e0b321d3b56523d8069791561241560d83399786
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-10-21 12:16:54 +0300

    Scripted auto-commit on change (2020-10-21 12:16:54) by gitwatch.sh

--- src/main/java/lab2/FlightMapper.java
+++ src/main/java/lab2/FlightMapper.java
@@ -7,7 +7,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlightMapper extends Mapper<LongWritable, Text, AirportID, Text>{
-    private static final int ARR_
+    private static final int DEST_AIRPORT_ID = 14;
     @Override
     protected void map(LongWritable key, Text value, Context context) {
 
