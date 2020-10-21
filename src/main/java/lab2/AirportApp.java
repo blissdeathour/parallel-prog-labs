e760aca58ab55b7cc446e1b413f5dd26039f0bd0
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-10-21 15:02:58 +0300

    Scripted auto-commit on change (2020-10-21 15:02:58) by gitwatch.sh

--- src/main/java/lab2/AirportApp.java
+++ src/main/java/lab2/AirportApp.java
@@ -2,6 +2,10 @@ package lab2;
 
 public class AirportApp {
     public static void main(String[] args) throws Exception {
-
+        if (args.length != 3)
+        {
+            System.exit(-1);
+        }
+        
     }
 }
