a15b79025fa9e8567f721b6652f4f08f3f76a8f4
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-10-21 14:24:20 +0300

    Scripted auto-commit on change (2020-10-21 14:24:20) by gitwatch.sh

--- src/main/java/lab2/AirportGroupingComparator.java
+++ src/main/java/lab2/AirportGroupingComparator.java
@@ -3,5 +3,8 @@ package lab2;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportGroupingComparator extends WritableComparator {
+    AirportGroupingComparator() {
+        super(AirportID.class, true);
+    }
     
 }
