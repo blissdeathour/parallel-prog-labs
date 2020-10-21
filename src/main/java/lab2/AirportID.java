9fb1be5ae7634cb68363fd248adb192291cbffd2
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-10-21 11:21:39 +0300

    Scripted auto-commit on change (2020-10-21 11:21:39) by gitwatch.sh

--- src/main/java/lab2/AirportID.java
+++ src/main/java/lab2/AirportID.java
@@ -2,6 +2,11 @@ package lab2;
 
 import org.apache.hadoop.io.WritableComparable;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOError;
+import java.io.IOException;
+
 public class AirportID implements WritableComparable<AirportID> {
     int     airportID;
     Boolean identifier;
@@ -15,5 +20,17 @@ public class AirportID implements WritableComparable<AirportID> {
     }
 
     @Override
-    public void write()
+    public void write(DataOutput output) throws IOException {
+        output.writeInt(airportID);
+        output.writeBoolean(identifier);
+    }
+
+    @Override
+    public void readFields(DataInput input) throws IOException {
+        airportID = input.readInt();
+        identifier = input.readBoolean();
+    }
+
+    @Override
+    public void compareTo()
 }
