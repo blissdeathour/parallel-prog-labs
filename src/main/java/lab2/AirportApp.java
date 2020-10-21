1560ea9ef7c5525e9be6cfe840352901ef056e77
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-10-21 15:16:54 +0300

    Scripted auto-commit on change (2020-10-21 15:16:54) by gitwatch.sh

--- src/main/java/lab2/AirportApp.java
+++ src/main/java/lab2/AirportApp.java
@@ -1,5 +1,6 @@
 package lab2;
 
+import com.sun.org.apache.bcel.internal.generic.MULTIANEWARRAY;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapred.TextInputFormat;
 import org.apache.hadoop.mapred.lib.MultipleInputs;
