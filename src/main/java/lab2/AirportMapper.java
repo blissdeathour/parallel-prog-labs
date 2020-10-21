b3bc9aaa901ad6fbcfb0169fab1c5536da9ebda6
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-10-21 11:46:54 +0300

    Scripted auto-commit on change (2020-10-21 11:46:54) by gitwatch.sh

--- src/main/java/lab2/AirportMapper.java
+++ src/main/java/lab2/AirportMapper.java
@@ -1,5 +1,16 @@
 package lab2;
 
-public class AirportMapper extends Mapper<> {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
+public class AirportMapper extends Mapper<LongWritable, Text, AirportID, IntWritable> {
+    @Override
+    public void map(LongWritable key, Text text, Context context) throws IOException,
+            InterruptedException {
+        
+    }
 }
