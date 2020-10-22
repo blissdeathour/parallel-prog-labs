3e2d700f73316071027743b58c064425761af43d
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-10-22 11:33:14 +0300

    Scripted auto-commit on change (2020-10-22 11:33:14) by gitwatch.sh

--- src/main/java/lab2/CalcReducer.java
+++ src/main/java/lab2/CalcReducer.java
@@ -15,6 +15,6 @@ public class CalcReducer extends Reducer<AirportID, Text, Text, Text> {
             return ;
         float minVal = Integer.MAX_VALUE;
         float maxVal - Integer.MIN_VALUE;
-        
+        String 
     }
 }
