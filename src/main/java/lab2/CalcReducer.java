964b73fd7c92535edfab9872671329c9a8197b82
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-10-22 11:30:06 +0300

    Scripted auto-commit on change (2020-10-22 11:30:06) by gitwatch.sh

--- src/main/java/lab2/CalcReducer.java
+++ src/main/java/lab2/CalcReducer.java
@@ -4,11 +4,13 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class CalcReducer extends Reducer<AirportID, Text, Text, Text> {
     @Override
     protected void reduce(AirportID key, Iterable<Text> values, Context context) throws IOException,
             InterruptedException {
+        Iterator<Text> iterator = values.iterator();
         
     }
 }
