5655b53d18090be97171a1f4f7dfd273dd616de6
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-10-22 11:03:36 +0300

    Scripted auto-commit on change (2020-10-22 11:03:36) by gitwatch.sh

--- src/main/java/lab2/AirportApp.java
+++ src/main/java/lab2/AirportApp.java
@@ -26,7 +26,7 @@ public class AirportApp {
 //        job.setGroupingComparatorClass(AirportGroupingComparator.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-        
+
         job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
