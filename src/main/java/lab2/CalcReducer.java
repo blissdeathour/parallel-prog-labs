af9c4da7c284d7d3d1cd19c85ed21337df53b53c
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-10-22 10:13:07 +0300

    Scripted auto-commit on change (2020-10-22 10:13:07) by gitwatch.sh

--- src/main/java/lab2/CalcReducer.java
+++ src/main/java/lab2/CalcReducer.java
@@ -1,5 +1,9 @@
 package lab2;
 
-public class CalcReducer extends Reducer<AirportID, {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
 
+public class CalcReducer extends Reducer<AirportID, Text, Text, Text> {
+    @Override
+    
 }
