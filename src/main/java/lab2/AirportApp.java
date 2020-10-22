70d5fcbbc801f0bfed228a48bcb67017e716dd56
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-10-22 12:01:08 +0300

    Scripted auto-commit on change (2020-10-22 12:01:08) by gitwatch.sh

--- src/main/java/lab2/AirportApp.java
+++ src/main/java/lab2/AirportApp.java
@@ -22,6 +22,7 @@ public class AirportApp {
 
         job.setMapOutputKeyClass(AirportID.class);
         job.setMapOutputValueClass(Text.class);
+        job.setReducerClass(CalcReducer.class);
 //        job.setPartitionerClass(AirportPartitioner.class);
 //        job.setGroupingComparatorClass(AirportGroupingComparator.class);
         job.setOutputKeyClass(Text.class);
