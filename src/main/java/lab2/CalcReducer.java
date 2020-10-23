7619e49d0d2c0de8c81558e5ed04dd4c1cb99732
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-10-23 11:02:30 +0300

    Scripted auto-commit on change (2020-10-23 11:02:30) by gitwatch.sh

--- src/main/java/lab2/CalcReducer.java
+++ src/main/java/lab2/CalcReducer.java
@@ -11,7 +11,7 @@ public class CalcReducer extends Reducer<AirportID, Text, Text, Text> {
     protected void reduce(AirportID key, Iterable<Text> values, Context context) throws IOException,
             InterruptedException {
         Iterator<Text>  iterator;
-        String          output;
+        String          output = "";
         float           minVal;
         float           maxVal;
         float           avg;
@@ -20,8 +20,8 @@ public class CalcReducer extends Reducer<AirportID, Text, Text, Text> {
         iterator = values.iterator();
         minVal = Integer.MAX_VALUE;
         maxVal = Integer.MIN_VALUE;
-        output = "Name: " + iterator.next().toString() + ", ";
-        System.out.printf("Name: %s \n", iterator.toString());
+//        output = "Name: " + iterator.next().toString() + ", ";
+        System.out.printf("Name: %s \n", iterator.next().toString());
         if (!iterator.hasNext())
             return ;
         avg = 0;
@@ -34,7 +34,7 @@ public class CalcReducer extends Reducer<AirportID, Text, Text, Text> {
             maxVal = Math.max(val, maxVal);
             avg = (avg * count + val) / ++count;
         }
-        output += String.format("Min: %f, Max: %f, Avg: %f", minVal, maxVal, avg);
+//        output += String.format("Min: %f, Max: %f, Avg: %f", minVal, maxVal, avg);
         context.write(new Text(Integer.toString(key.airportID)), new Text(output));
     }
 }
