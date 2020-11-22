34d99d0d39f9c6e4aa8a7df747ef082455b80b68
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-11-22 16:20:39 +0300

    Scripted auto-commit on change (2020-11-22 16:20:39) by gitwatch.sh

--- src/main/java/lab3/FlightData.java
+++ src/main/java/lab3/FlightData.java
@@ -1,5 +1,19 @@
 package lab3;
 
 public class FlightData {
-    
+    public Float minDelay;
+    public Integer totCount;
+    public Integer wrongCount;
+
+    public FlightData(String row) {
+        totCount = 1;
+        if (row.isEmpty()) {
+            minDelay = 0.f;
+            wrongCount = 1;
+        }
+        else {
+            minDelay = Float.parseFloat(row);
+            wrongCount = minDelay >= 
+        }
+    }
 }
