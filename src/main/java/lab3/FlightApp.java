87788f6fff12d111471c1fa03453b671761c6941
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-11-22 15:38:00 +0300

    Scripted auto-commit on change (2020-11-22 15:38:00) by gitwatch.sh

--- src/main/java/lab3/FlightApp.java
+++ src/main/java/lab3/FlightApp.java
@@ -1,5 +1,22 @@
 package lab3;
 
-public class FlightApp {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 
+public class FlightApp {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 3) {
+            System.exit(-1);
+        }
+
+        SparkConf cf = new SparkConf().setAppName("FlightApp");
+        JavaSparkContext sc = new JavaSparkContext(cf);
+
+        JavaPairRDD<LongWritable, Text> airportFile = sc.hadoopFile(args[0], TextInputFormat.class, LongWritable.class, Text.class);
+        JavaPairRDD<Long, Long> airport
+    }
 }
