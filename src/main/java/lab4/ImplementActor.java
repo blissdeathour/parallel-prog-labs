6d4de7bfca06421e86a7c9cfc612f9cdbbf637b2
Franz Kemmerich <franzkemmerich@192.168.1.10>
2020-12-19 22:39:00 +0300

    Scripted auto-commit on change (2020-12-19 22:39:00) by gitwatch.sh

--- src/main/java/lab4/ImplementActor.java
+++ src/main/java/lab4/ImplementActor.java
@@ -3,6 +3,7 @@ package lab4;
 import akka.actor.AbstractActor;
 import lab4.TestsResult;
 import lab4.execMsg;
+import lab4.execRequest;
 
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
@@ -12,6 +13,12 @@ public class ImplementActor extends AbstractActor {
     private final static String ENGINE_NAME = "nashorn";
 
     private static TestsResult execTests(execMsg msg) throws Exception {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName(ENGINE_NAME);
+        engine.eval(msg.getCode());
 
+        Invocable invocable = (Invocable) engine;
+        TestsResult testsResult = new TestsResult();
+
+        for (ExecuteRequs)
     }
 }
