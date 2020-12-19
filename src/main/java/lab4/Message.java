dbb9f48aea4d0b5b16d184e85645c7d8b6ebe759
Franz Kemmerich <franzkemmerich@192.168.1.10>
2020-12-20 00:45:08 +0300

    Scripted auto-commit on change (2020-12-20 00:45:08) by gitwatch.sh

--- src/main/java/lab4/Message.java
+++ src/main/java/lab4/Message.java
@@ -59,28 +59,3 @@ public static void getPubID() { return this.pubID; }
         return (builder.deleteCharAt(builder.length() - 1).append(}).toString());
     }
 public static void getPubID() { return this.pubID; }
-    public String toJSON() {
-        for (Map.Entry<String, Boolean> res : results.entrySet()) {
-            builder.append(String.format(%s:%s, res.getKey(), res.getValue() ? true :
-	    false));
-        }
-        return (builder.deleteCharAt(builder.length() - 1).append(}).toString());
-    }
-public static void getPubID() { return this.pubID; }
-    public String toJSON() {
-        for (Map.Entry<String, Boolean> res : results.entrySet()) {
-            builder.append(String.format(%s:%s, res.getKey(), res.getValue() ? true :
-	    false));
-        }
-        return (builder.deleteCharAt(builder.length() - 1).append(}).toString());
-    }
-public static void getPubID() { return this.pubID; }
-    public String toJSON() {
-        for (Map.Entry<String, Boolean> res : results.entrySet()) {
-            builder.append(String.format(%s:%s, res.getKey(), res.getValue() ? true :
-	    false));
-        }
-        return (builder.deleteCharAt(builder.length() - 1).append(}).toString());
-    }
-public static void getPubID() { return this.pubID; }
-public static void getPubID() { return this.pubID; }
