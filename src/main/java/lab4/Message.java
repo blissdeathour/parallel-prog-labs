b3c76d66afc3b77e32fc49d7b8fae03049c05357
Franz Kemmerich <franzkemmerich@192.168.1.10>
2020-12-20 00:32:57 +0300

    Scripted auto-commit on change (2020-12-20 00:32:57) by gitwatch.sh

--- src/main/java/lab4/Message.java
+++ src/main/java/lab4/Message.java
@@ -83,3 +83,4 @@ public static void getPubID() { return this.pubID; }
         return (builder.deleteCharAt(builder.length() - 1).append(}).toString());
     }
 public static void getPubID() { return this.pubID; }
+public static void getPubID() { return this.pubID; }
