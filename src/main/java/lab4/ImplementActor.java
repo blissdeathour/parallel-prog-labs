a591fc01f5aeb16c5a87b0124aa3b4ece0f5807f
Franz Kemmerich <franzkemmerich@192.168.1.10>
2020-12-19 22:35:03 +0300

    Scripted auto-commit on change (2020-12-19 22:35:03) by gitwatch.sh

--- src/main/java/lab4/ImplementActor.java
+++ src/main/java/lab4/ImplementActor.java
@@ -18,6 +18,6 @@ public class ImplementActor extends AbstractActor {
         Invocable invocable = (Invocable) engine;
         TestsResult testsResult = new TestsResult();
 
-        for (E)
+        for (ExecuteRequs)
     }
 }
