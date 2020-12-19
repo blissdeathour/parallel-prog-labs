693b9bc6dae48e9668e0513ffa44dd13ad56bd96
Franz Kemmerich <franzkemmerich@192.168.1.10>
2020-12-19 22:25:45 +0300

    Scripted auto-commit on change (2020-12-19 22:25:45) by gitwatch.sh

--- src/main/java/lab4/TestsResult.java
+++ src/main/java/lab4/TestsResult.java
@@ -5,10 +5,22 @@ import java.util.Map;
 
 public class TestsResult {
     private Map<String, Boolean> results;
+    private StringBuilder builder;
 
     public TestsResult() {
         results = new HashMap<>();
+        builder = new StringBuilder();
     }
 
-    public S
+    public String toJSON() {
+        for (Map.Entry<String, Boolean> res : results.entrySet()) {
+            builder.append(String.format("%s:%s", res.getKey(), res.getValue() ? "true" : "false"));
+        }
+        return (builder.deleteCharAt(builder.length() - 1).append("}").toString());
+    }
+
+    @Override
+    public String toString() {
+        for ()
+    }
 }
