c3a23ca68eaf26bb686be7b85b64aa1cd61367e1
Franz Kemmerich <franzkemmerich@192.168.1.10>
2020-12-19 16:57:19 +0300

    Scripted auto-commit on change (2020-12-19 16:57:19) by gitwatch.sh

--- src/main/java/lab4/ImplementActor.java
+++ src/main/java/lab4/ImplementActor.java
@@ -1,7 +1,9 @@
 package lab4;
 
-public class ImplementActor {
-    public static String executeTests(ExecuteMessage msg) throws Exception {
+import akka.actor.AbstractActor;
 
+public class ImplementActor extends AbstractActor {
+    public static String executeTests(ExecuteMessage msg) throws Exception {
+        
     }
 }
