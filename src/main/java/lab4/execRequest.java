8b1892c670712e66e9e751ce486ef61a8e4dbc6d
Franz Kemmerich <franzkemmerich@192.168.1.10>
2020-12-19 20:08:18 +0300

    Scripted auto-commit on change (2020-12-19 20:08:18) by gitwatch.sh

--- src/main/java/lab4/execRequest.java
+++ src/main/java/lab4/execRequest.java
@@ -1,2 +1,15 @@
-package lab4;public class execRequest {
+package lab4;
+
+public class execRequest {
+    public String packageID;
+    public String jsScript;
+    public String funcName;
+
+    public execRequest(String packageID, String jsScript, String funcName) {
+        this.packageID = packageID;
+        this.jsScript = jsScript;
+        this.funcName = funcName;
+    }
+
+
 }
