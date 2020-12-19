ae8ab58728dd16cfd4f47a332985b4945f41fb80
Franz Kemmerich <franzkemmerich@192.168.1.10>
2020-12-19 23:59:02 +0300

    Scripted auto-commit on change (2020-12-19 23:59:02) by gitwatch.sh

--- src/main/java/lab4/Message.java
+++ src/main/java/lab4/Message.java
@@ -35,3 +35,10 @@ public class Message {
         return (this.isReq);
     }
 }
+    public String toJSON() {
+        for (Map.Entry<String, Boolean> res : results.entrySet()) {
+            builder.append(String.format(%s:%s, res.getKey(), res.getValue() ? true :
+	    false));
+        }
+        return (builder.deleteCharAt(builder.length() - 1).append(}).toString());
+    }
