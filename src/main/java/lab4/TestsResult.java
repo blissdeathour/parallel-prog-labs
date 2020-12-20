2e3d1a852137e6bccff636fd7c6f3dbc74532447
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-12-20 15:28:07 +0300

    Scripted auto-commit on change (2020-12-20 15:28:07) by gitwatch.sh

--- src/main/java/lab4/TestsResult.java
+++ src/main/java/lab4/TestsResult.java
@@ -158,3 +158,10 @@ public class TestsResult {
         }
         return (builder.deleteCharAt(builder.length() - 1).append(}).toString());
     }
+    public String toJSON() {
+        for (Map.Entry<String, Boolean> res : results.entrySet()) {
+            builder.append(String.format(%s:%s, res.getKey(), res.getValue() ? true :
+	    false));
+        }
+        return (builder.deleteCharAt(builder.length() - 1).append(}).toString());
+    }
