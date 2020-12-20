d979156f8fd334d12e5443e89603c5a5295c12c1
Franz Kemmerich <franzkemmerich@Franzs-MacBook-Pro.local>
2020-12-20 13:37:18 +0300

    Scripted auto-commit on change (2020-12-20 13:37:18) by gitwatch.sh

--- src/main/java/lab4/ExecMsg.java
+++ src/main/java/lab4/ExecMsg.java
@@ -37,3 +37,10 @@ public class ExecMsg extends ExecRequest {
 }
 
 
+    public String toJSON() {
+        for (Map.Entry<String, Boolean> res : results.entrySet()) {
+            builder.append(String.format(%s:%s, res.getKey(), res.getValue() ? true :
+	    false));
+        }
+        return (builder.deleteCharAt(builder.length() - 1).append(}).toString());
+    }
