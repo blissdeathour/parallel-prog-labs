3ea160071f4dbb0f8c7ce452824dbe3f9ff93223
Sergey <nesonsv1@gmail.com>
2020-12-22 20:39:48 +0300

    Scripted auto-commit on change (2020-12-22 20:39:48) by gitwatch.sh

--- src/main/java/lab6/ServersList.java
+++ src/main/java/lab6/ServersList.java
@@ -1,5 +1,15 @@
 package lab6;
 
+import java.util.List;
+
 public class ServersList {
-    
+    private List<String> servers;
+
+    public ServersList(List<String> servers) {
+        this.servers = servers;
+    }
+
+    public List<String> getServers() {
+        return (this.servers);
+    }
 }
