85102358437f01128eb84ae7a71dc3ebbbd4d01c
Sergey <nesonsv1@gmail.com>
2020-12-22 16:01:11 +0300

    Scripted auto-commit on change (2020-12-22 16:01:11) by gitwatch.sh

--- src/main/java/lab4/TestApp.java
+++ src/main/java/lab4/TestApp.java
@@ -4,8 +4,22 @@ import akka.actor.Actor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.model.StatusCodes;
 import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
+import akka.util.Timeout;
+import lab4.msgs.ExecMsg;
+import lab4.msgs.ExecRequest;
+import lab4.msgs.GetMsg;
+import lab4.msgs.StoreMsg;
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+
+
+import java.time.Duration;
+import java.util.concurrent.TimeoutException;
 
 import static akka.http.javadsl.server.Directives.*;
 
@@ -13,6 +27,7 @@ import static akka.http.javadsl.server.Directives.*;
 public class TestApp {
     ActorSystem actorSystem;
     ActorRef routeActor;
+    private static final Timeout timeout = Timeout.create(Duration.ofSeconds(5));
 
     public static void main(String[] args) throws Exception {
         ActorSystem system = ActorSystem.create("TestApp");
@@ -24,6 +39,21 @@ public class TestApp {
 
     public Route getRoute() {
         path("execute", () -> route(post(() ->
-                en)))
+                entity(Jackson.unmarshaller(ExecRequest.class), b -> {
+                    routeActor.tell(new ExecMsg(b.getPackageID(), b.getJsScript(), b.getFuncName(), b.getTests()),
+                            ActorRef.noSender());
+                    return (complete(StatusCodes.OK, String.format("Testing for %s started\n", b.getPackageID())));
+                })))),
+        path("retreive", () -> route(get(() ->
+                parameter("packageID", pID -> {
+                    Future<Object> future = Patterns.ask(routeActor, new GetMsg(pID), timeout);
+                    StoreMsg res;
+                    try {
+                        res = (StoreMsg) Await.result(future, timeout.duration());
+                    } catch (TimeoutException | InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    
+                }))))
     }
 }
