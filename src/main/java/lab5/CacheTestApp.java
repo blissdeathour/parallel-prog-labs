d588995a59793022d629f59b5126bde13061fbcd
Sergey <nesonsv1@gmail.com>
2020-12-22 18:05:17 +0300

    Scripted auto-commit on change (2020-12-22 18:05:17) by gitwatch.sh

--- src/main/java/lab5/CacheTestApp.java
+++ src/main/java/lab5/CacheTestApp.java
@@ -1,5 +1,24 @@
 package lab5;
 
-public class CacheTestApp {
-    
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import lab5.actors.RouteActor;
+import org.spark_project.jetty.client.HttpRequest;
+import org.spark_project.jetty.client.HttpResponse;
+
+public class CacheTestApp{
+
+
+    public static void main(String[] args) throws Exception {
+        ActorSystem system = ActorSystem.create("CacheTestApp");
+        ActorRef actorRef = system.actorOf(Props.create(RouteActor.class));
+        Http http = Http.get(system);
+        ActorMaterializer materializer = ActorMaterializer.create(system);
+        Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = 
+    }
 }
