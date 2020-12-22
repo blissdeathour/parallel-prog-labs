8e022e2c64667ebac2ec71d401f789e1b9fb66eb
Sergey <nesonsv1@gmail.com>
2020-12-22 14:55:30 +0300

    Scripted auto-commit on change (2020-12-22 14:55:30) by gitwatch.sh

--- src/main/java/lab4/RouterActor.java
+++ src/main/java/lab4/RouterActor.java
@@ -3,10 +3,16 @@ package lab4;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 import akka.actor.ActorRef;
+import org.junit.Before;
 
 public class RouterActor extends AbstractActor {
     private ActorRef execActor;
     private ActorRef storeActor;
+
+    @Before
+    public void init() {
+
+    }
 
     @Override
     public Receive createReceive() {
