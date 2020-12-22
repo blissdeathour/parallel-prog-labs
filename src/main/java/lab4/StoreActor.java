4471b2aac0ada8b402a365351e735de28a603884
Sergey <nesonsv1@gmail.com>
2020-12-22 14:29:50 +0300

    Scripted auto-commit on change (2020-12-22 14:29:50) by gitwatch.sh

--- src/main/java/lab4/StoreActor.java
+++ src/main/java/lab4/StoreActor.java
@@ -1,2 +1,16 @@
-package lab4;public class StoreActor {
+package lab4;
+
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StoreActor extends AbstractActor {
+    private static final Map<String, TestsResult> testsResults = new HashMap<>();
+
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.
+    }
 }
