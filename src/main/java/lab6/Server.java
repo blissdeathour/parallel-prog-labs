d3cfa69b598632fec81f536e4818184c24288b37
Sergey <nesonsv1@gmail.com>
2020-12-22 20:36:28 +0300

    Scripted auto-commit on change (2020-12-22 20:36:28) by gitwatch.sh

--- src/main/java/lab6/Server.java
+++ src/main/java/lab6/Server.java
@@ -1,5 +1,13 @@
 package lab6;
 
 public class Server {
-    
+    private String url;
+
+    public Server(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return (this.url);
+    }
 }
