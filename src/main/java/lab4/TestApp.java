7021d046bdda109ef80d2268a15b677f29491cfa
Sergey <nesonsv1@gmail.com>
2020-12-22 16:09:27 +0300

    Scripted auto-commit on change (2020-12-22 16:09:27) by gitwatch.sh

--- src/main/java/lab4/TestApp.java
+++ src/main/java/lab4/TestApp.java
@@ -3,6 +3,7 @@ package lab4;
 import akka.actor.Actor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.StatusCodes;
@@ -10,6 +11,7 @@ import akka.http.javadsl.server.Route;
 import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.util.Timeout;
+import lab4.actors.RouterActor;
 import lab4.msgs.ExecMsg;
 import lab4.msgs.ExecRequest;
 import lab4.msgs.GetMsg;
@@ -34,26 +36,38 @@ public class TestApp {
 
         Http http = Http.get(system);
         ActorMaterializer materializer = ActorMaterializer.create(system);
+        init(system);
+    }
 
+    public void init(ActorSystem sys) {
+        actorSystem = sys;
+        routeActor = actorSystem.actorOf(Props.create(RouterActor.class));
     }
 
     public Route getRoute() {
-        path("execute", () -> route(post(() ->
-                entity(Jackson.unmarshaller(ExecRequest.class), b -> {
-                    routeActor.tell(new ExecMsg(b.getPackageID(), b.getJsScript(), b.getFuncName(), b.getTests()),
-                            ActorRef.noSender());
-                    return (complete(StatusCodes.OK, String.format("Testing for %s started\n", b.getPackageID())));
-                })))),
-        path("retreive", () -> route(get(() ->
-                parameter("packageID", pID -> {
-                    Future<Object> future = Patterns.ask(routeActor, new GetMsg(pID), timeout);
-                    StoreMsg res;
-                    try {
-                        res = (StoreMsg) Await.result(future, timeout.duration());
-                    } catch (TimeoutException | InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    
-                }))))
+        return route(
+                path("execute", () -> route(post(() ->
+                        entity(Jackson.unmarshaller(ExecRequest.class), b -> {
+                            routeActor.tell(new ExecMsg(b.getPackageID(), b.getJsScript(), b.getFuncName(), b.getTests()),
+                                    ActorRef.noSender());
+                            return (complete(StatusCodes.OK, String.format("Testing for %s started\n", b.getPackageID())));
+                        })))),
+                path("retreive", () -> route(get(() ->
+                        parameter("packageID", pID -> {
+                            Future<Object> future = Patterns.ask(routeActor, new GetMsg(pID), timeout);
+                            StoreMsg res;
+                            try {
+                                res = (StoreMsg) Await.result(future, timeout.duration());
+                            } catch (TimeoutException | InterruptedException e) {
+                                e.printStackTrace();
+                                return (complete(StatusCodes.INTERNAL_SERVER_ERROR, "Error\n"));
+                            }
+                            if (res != null && res.getRes() != null) {
+                                return (complete(StatusCodes.OK, res.getRes().toJSON() + "\n"));
+                            }
+                            else {
+                                return (complete(StatusCodes.OK, res.getRes().toJSON() + "\n"));
+                            }
+                        })))), complete(StatusCodes.NOT_FOUND, "Bad request\n"));
     }
 }
