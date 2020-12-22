60846defd5c4ce16a92b55249e7032bb4bea583a
Sergey <nesonsv1@gmail.com>
2020-12-22 15:46:46 +0300

    Scripted auto-commit on change (2020-12-22 15:46:46) by gitwatch.sh

--- src/main/java/lab4/TestApp.java
+++ src/main/java/lab4/TestApp.java
@@ -1,17 +1,22 @@
 package lab4;
 
 import akka.actor.Actor;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
 import akka.stream.ActorMaterializer;
 
 public class TestApp {
+    ActorSystem actorSystem;
+    ActorRef routeActor;
 
     public static void main(String[] args) throws Exception {
         ActorSystem system = ActorSystem.create("TestApp");
 
         Http http = Http.get(system);
         ActorMaterializer materializer = ActorMaterializer.create(system);
-        
+
     }
+
+    public Route getRoute
 }
