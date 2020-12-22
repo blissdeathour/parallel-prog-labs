52b8f15d0a88fa27e2e6d68c82a7ea207133b1a2
Sergey <nesonsv1@gmail.com>
2020-12-22 16:21:43 +0300

    Scripted auto-commit on change (2020-12-22 16:21:43) by gitwatch.sh

--- src/main/java/lab4/TestApp.java
+++ src/main/java/lab4/TestApp.java
@@ -24,7 +24,7 @@ import lab4.msgs.GetMsg;
 import lab4.msgs.StoreMsg;
 import scala.concurrent.Await;
 import scala.concurrent.Future;
-
+import java.util.concurrent.CompletionStage;
 
 import java.time.Duration;
 import java.util.concurrent.TimeoutException;
@@ -37,7 +37,7 @@ public class TestApp {
     static ActorRef routeActor;
     private static final Timeout timeout = Timeout.create(Duration.ofSeconds(5));
     private final static String IP = "localhost";
-    private final static String PORT = "8080";
+    private final static int PORT = 8080;
 
     public static void main(String[] args) throws Exception {
         ActorSystem system = ActorSystem.create("TestApp");
@@ -71,34 +71,11 @@ public class TestApp {
                             }
                         })))), complete(StatusCodes.NOT_FOUND, "Bad request\n")).flow(system, materializer);
         CompletionStage<ServerBinding> binding = http.bindAndHandle(
-                routeFlow, ConnectHttp.toHost(IP, PORT);
+                routeFlow, ConnectHttp.toHost(IP, PORT), materializer
         );
-    }
 
-    public Route getRoute() {
-        return route(
-                path("execute", () -> route(post(() ->
-                        entity(Jackson.unmarshaller(ExecRequest.class), b -> {
-                            routeActor.tell(new ExecMsg(b.getPackageID(), b.getJsScript(), b.getFuncName(), b.getTests()),
-                                    ActorRef.noSender());
-                            return (complete(StatusCodes.OK, String.format("Testing for %s started\n", b.getPackageID())));
-                        })))),
-                path("retreive", () -> route(get(() ->
-                        parameter("packageID", pID -> {
-                            Future<Object> future = Patterns.ask(routeActor, new GetMsg(pID), timeout);
-                            StoreMsg res;
-                            try {
-                                res = (StoreMsg) Await.result(future, timeout.duration());
-                            } catch (TimeoutException | InterruptedException e) {
-                                e.printStackTrace();
-                                return (complete(StatusCodes.INTERNAL_SERVER_ERROR, "Error\n"));
-                            }
-                            if (res != null && res.getRes() != null) {
-                                return (complete(StatusCodes.OK, res.getRes().toJSON() + "\n"));
-                            }
-                            else {
-                                return (complete(StatusCodes.OK, res.getRes().toJSON() + "\n"));
-                            }
-                        })))), complete(StatusCodes.NOT_FOUND, "Bad request\n"));
+        System.out.printf("Server listening...");
+        System.in.read();
+        binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
     }
 }
