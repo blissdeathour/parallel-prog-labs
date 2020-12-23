48e481f4c94c6fabfe206df9a99b801d5a5ccf9e
Sergey <nesonsv1@gmail.com>
2020-12-23 18:32:44 +0300

    Scripted auto-commit on change (2020-12-23 18:32:43) by gitwatch.sh

--- src/main/java/lab6/ZooKeeper.java
+++ src/main/java/lab6/ZooKeeper.java
@@ -1,9 +1,13 @@
 package lab6;
 
 import akka.actor.ActorRef;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.Watcher;
 
 public class ZooKeeper implements Watcher {
+    private ActorRef storage;
 
-    public ZooKeeper(ActorRef storage) 
+    public ZooKeeper(ActorRef storage) throws KeeperException, InterruptedException {
+
+    }
 }
